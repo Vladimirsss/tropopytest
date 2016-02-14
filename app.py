8700f224118ec4618a6807e084a1820ee5bbbc98
Vladimirsss <vsazonov@cisco.com>
2016-02-14 14:01:57 +0000

    1

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+#from flask import Flask
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!!"
+#@app.route("/")
+#def hello():
+#    return "Hello World!!"
+
+#if __name__ == "__main__":
+#    app.run(debug=True,host='0.0.0.0')
 
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+import requests
+from urllib.request import Request, urlopen
+import json
+import time
+
+
+print("<<<start>>>")
+
+myroomid = "Y2lzY29zcGFyazovL3VzL1JPT00vYjE4YzJlMjAtY2ZjMS0xMWU1LWJmY2YtZTE4YTBkMzMwYTg1"
+sheaders = {
+    "Authorization": "Bearer M2RkNzJjNGQtY2UwNS00NGJkLThiOTgtNmQ2Y2VjYzdjMDI0YWY3N2ZjZGEtYzUz",
+    "Content-type": "application/json"}
+
+
+requestURL="https://api.ciscospark.com/v1/messages"+"?"+"roomId="+str(myroomid)+"&"+"max=1"
+
+responseString=requests.get(requestURL,headers=sheaders)
+
+ro=responseString.json()
+
+print("1> ", ro["items"][0]["text"])
+
